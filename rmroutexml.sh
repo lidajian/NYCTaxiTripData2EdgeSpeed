3832956a11655708215e3851e747d07fd51c6a23
lidajian <li.dajian@foxmail.com>
2016-03-10 23:54:59 +0800

    rmroutexml tool removes route.xml to save space

---
+++ rmroutexml.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+cd $1"01"
+rm route.xml
+echo "finish 1"
+cd "../"$1"02"
+rm route.xml
+echo "finish 1"
+cd "../"$1"03"
+rm route.xml
+echo "finish 1"
+cd "../"$1"04"
+rm route.xml
+echo "finish 1"
+cd "../"$1"05"
+rm route.xml
+echo "finish 1"
+cd "../"$1"06"
+rm route.xml
+echo "finish 1"
+cd "../"$1"07"
+rm route.xml
+echo "finish 1"
+cd "../"$1"08"
+rm route.xml
+echo "finish 1"
+cd "../"$1"09"
+rm route.xml
+echo "finish 1"
+cd "../"$1"10"
+rm route.xml
+echo "finish 1"
+cd "../"$1"11"
+rm route.xml
+echo "finish 1"
+cd "../"$1"12"
+rm route.xml
+echo "finish 1"
